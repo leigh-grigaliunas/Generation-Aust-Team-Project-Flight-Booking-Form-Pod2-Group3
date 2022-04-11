548c34faef1c349648826bfb052e342002e0c831
leigh-grigaliunas <leigh.grigaliunas@gmail.com>
2022-04-12 09:05:47 +1000

    Added initial javascript

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+// Main dom element requirements  
+
+const passengerContainer = document.getElementById(‘passenger-details’);
+const paymentContainer = document.getElementById(‘papment-details’);
+const flightContainer = document.getElementById(‘flight-details’);
+
+const flightBtn = document.getElementById(‘flight);
+const passengerBtn = document.getElementById(‘passengers’);
+const paymentBtn = document.getElementById('payment’);
+
+Main validation
