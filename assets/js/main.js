d038e0615549d4ea4a313cfb4aec0f9589a936c4
leigh-grigaliunas <leigh.grigaliunas@gmail.com>
2022-04-12 11:40:11 +1000

    adding in functions for validation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,61 @@
-// Main dom element requirements  
+// Main DOM element gets  
 
-const passengerContainer = document.getElementById(‘passenger-details’);
-const paymentContainer = document.getElementById(‘papment-details’);
-const flightContainer = document.getElementById(‘flight-details’);
+const passengerContainer = document.getElementById("passenger-details");
+const paymentContainer = document.getElementById("payment-details");
+const flight = document.getElementById("flight-details");
+const adults = document.getElementById("adults-travelling");
+const children = document.getElementById("children-travelling");
+
 
 const flightBtn = document.getElementById(‘flight);
 const passengerBtn = document.getElementById(‘passengers’);
 const paymentBtn = document.getElementById('payment’);
 
-Main validation
+// Main validation functions
+
+
+
+
+function addPassenger(type){
+
+
+}
+
+function getPassengerNumbers(){
+	const totalTravelling = adults.options[adult.selectedIndex] + children.options[children.selectedIndex];
+    if (totalTravelling){
+        return totalTravelling
+    } else {
+        // throw error
+    }
+}
+
+function getPassengerDetails () {
+    
+    const travelling = getPassengerNumbers();
+    var i = travelling;
+
+    if (travelling){
+        while (i > 0){
+
+            
+        }
+
+
+    }
+    
+    If I < adults
+        addPassenger (adult)
+    Else
+        addPassenger (child)
+    End
+    
+    
+    
+    Build html string
+    Output html string
+
+function checkFlightType(){
+
+}
+
